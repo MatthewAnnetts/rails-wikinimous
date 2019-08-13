9122ab7df7353f3ec5e592b3a58ec5f4437202f3
matt annetts <mot182@hotmail.com>
2019-08-13 16:43:05 +0100

    challenge complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  article = Article.new(
+    title: Faker::Internet.username(specifier: 8),
+    content: Faker::Internet.domain_name)
+  article.save
+end
